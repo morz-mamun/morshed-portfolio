a65ca0dda58169f8d02c016f6070ab6ab747a68c
mamun <morzmamun@gmail.com>
2025-05-23 22:36:33 +0600

    deleted unused code

--- utils/scrollToSection.ts
+++ utils/scrollToSection.ts
@@ -1,7 +1,7 @@
 export const scrollToSection = (id: string) => {
     const section = document.getElementById(id);
     if (section) {
-        const offset = 100; // Adjust this value as needed
+        const offset = 100;
         const sectionPosition = section.getBoundingClientRect().top + window.scrollY;
         window.scrollTo({ top: sectionPosition - offset, behavior: "smooth" });
     }
