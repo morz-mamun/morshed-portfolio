20a79e217e3692724390bf6b78d08d52366c2071
mamun <morzmamun@gmail.com>
2025-06-01 13:03:17 +0600

    feat: add favicon and meta title

--- app/(public)/projects/page.tsx
+++ app/(public)/projects/page.tsx
@@ -1,4 +1,11 @@
 import ProjectPage from "@/pages/project-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Morshed | Projects',
+  description:
+    'Explore a collection of Morshed’s featured web development projects and creative work.',
+}
 export default function Projects() {
   return (
     <ProjectPage />
